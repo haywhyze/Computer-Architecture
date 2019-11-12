4b698c511ef9a0cab7488e50487946a5e26396b4
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-11-12 15:07:10 +0100

    unhardcode the machine code  and implement load function to load files dynamically

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,26 +17,56 @@ class CPU:
         # Program Counter
         self.pc = self.register[0]
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
+        if filename[-4:] != ".ls8":
+            full_filename = f"examples/{filename}.ls8"
+        else: full_filename = f"examples/{filename}"
+        try:
+            address = 0
+            with open(full_filename) as f:
+                for line in f:
+                    # deal with comments
+                    # split before and after any comment symbol '#'
+                    comment_split = line.split("#")
+
+                    # convert the pre-comment portion (to the left) from binary to a value
+                    # extract the first part of the split to a number variable
+                    # and trim whitespace
+                    num = comment_split[0].strip()
+
+                    # ignore blank lines / comment only lines
+                    if len(num) == 0:
+                        continue
+
+                    # set the number to an integer of base 2
+                    value = int(num, 2)
+                    # print the value in binary and in decimal
+                    # print(f"{value:08b}: {value:d}")
+                    
+                    # add the value in to the memory at the index of address
+                    self.ram[address] = value
+
+                    # increment the address
+                    address += 1
+
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
     def ram_read(self, location):
         """Read avalue stored at specified address."""
